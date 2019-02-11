c47787d8566c350208e7ea555c78bbf5569a8f91
Cristina-F <k.fefelova@g.nsu.ru>
2019-02-12 00:32:08 +0700

    changed brackets code style

--- GuessingGame/GuessingGame/GuessingGame.cs
+++ GuessingGame/GuessingGame/GuessingGame.cs
@@ -1,134 +1,118 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace GuessingGame
-{
-    internal class Guesser
-    {
-        private static void Main()
-        {
+namespace GuessingGame{
+    internal class Guesser{
+        private static void Main(){
             var guesser = new Guesser();
-             guesser.Play();  
+            guesser.Play();
         }
 
-        private void Play()
-        {
+        private void Play(){
             Console.WriteLine("Please, enter your name(:");
             _userName = Console.ReadLine();
             var isCorrectAnswer = false;
             Console.WriteLine("Try to guess my number, time has gone))))))");
             _startTime = DateTime.Now;
-            while (!isCorrectAnswer)
-            {
+            while (!isCorrectAnswer){
                 var userAnswer = Console.ReadLine();
-                if (userAnswer != null && userAnswer.Equals(ExitCode))
-                {
+                if (userAnswer != null && userAnswer.Equals(ExitCode)){
                     Console.WriteLine("I apologize and finish.");
                     return;
                 }
                 var userNumber = GetUserNumber(userAnswer);
                 isCorrectAnswer = IsCorrectAnswer(userNumber);
-                if (!isCorrectAnswer)
-                {
-                    CheckNumberOfGuesses();  
-                }  
+                if (!isCorrectAnswer){
+                    CheckNumberOfGuesses();
+                }
             }
         }
 
-        private void CheckNumberOfGuesses()
-        {
+        private void CheckNumberOfGuesses(){
             if (_guessingCounter % 4 != 0) return;
             var random = new Random();
             var i = random.Next(0, _motivatingPhrases.Length);
             Console.WriteLine($"{_userName}, {_motivatingPhrases[i]}");
         }
 
-        private bool IsCorrectAnswer(int userNumber)
-        {
+        private bool IsCorrectAnswer(int userNumber){
             _guessingCounter++;
-            if (userNumber.Equals(_randomNumber))
-            {
+            if (userNumber.Equals(_randomNumber)){
                 _endTime = DateTime.Now;
                 _history.Add(new KeyValuePair<int, Status>(userNumber, Status.CorrectNumber));
                 PrintGameInfo();
                 return true;
             }
 
-            if (userNumber < _randomNumber)
-            {
+            if (userNumber < _randomNumber){
                 _history.Add(new KeyValuePair<int, Status>(userNumber, Status.SmallNumber));
-                Console.WriteLine( _comparisonPhrases[(int) Status.SmallNumber]);
+                Console.WriteLine(_comparisonPhrases[(int) Status.SmallNumber]);
             }
-            else
-            {
+            else{
                 _history.Add(new KeyValuePair<int, Status>(userNumber, Status.BigNumber));
-                Console.WriteLine( _comparisonPhrases[(int) Status.BigNumber]);
+                Console.WriteLine(_comparisonPhrases[(int) Status.BigNumber]);
             }
+
             return false;
         }
-        private void PrintGameInfo()
-        {
+
+        private void PrintGameInfo(){
             Console.WriteLine($"Number of guessing attempts: {_guessingCounter}");
-            foreach (var (userNumber, status) in _history)
-            {
+            foreach (var (userNumber, status) in _history){
                 Console.WriteLine($"{userNumber} - {_comparisonPhrases[(int) status]}");
             }
-            var time = _endTime - _startTime;   
+
+            var time = _endTime - _startTime;
             Console.WriteLine($"Play time:  {time.Minutes}:{time.Seconds}");
         }
-        
-        private int GetUserNumber(string userAnswer)
-        {
+
+        private int GetUserNumber(string userAnswer){
             var userNumber = 0;
-            try
-            {
+            try{
                 userNumber = int.Parse(userAnswer);
                 return userNumber;
             }
-            catch
-            {
+            catch(Exception){
                 Console.WriteLine($"{_userName}, you entered nonsense.....");
                 GetUserNumber(Console.ReadLine());
             }
             return userNumber;
         }
-        
-        private Guesser()
-        {
+
+        private Guesser(){
             var random = new Random();
             _randomNumber = random.Next(MinRandomNumber, MaxRandomNumber);
             Console.WriteLine($"Secret random number = {_randomNumber}");
         }
-        
+
         private int _guessingCounter;
         private string _userName;
         private readonly int _randomNumber;
         private readonly List<KeyValuePair<int, Status>> _history = new List<KeyValuePair<int, Status>>();
         private DateTime _startTime;
         private DateTime _endTime;
-        
+
         private const int MinRandomNumber = 0;
         private const int MaxRandomNumber = 51;
         private const string ExitCode = "q";
-        private readonly string[] _comparisonPhrases =
-        {
+
+        private readonly string[] _comparisonPhrases = {
             "your number is too small):",
             "your number is too big):",
             "your number is CORRECT!!!!"
         };
-        private readonly string[] _motivatingPhrases =
-        {
+
+        private readonly string[] _motivatingPhrases = {
             "come on, you almost guessed!",
             "you can I believe in you(;",
             "you\'re not stupid, let\'s guess!",
             "try hard!"
         };
-        private enum Status
-        {
+
+        private enum Status{
             SmallNumber,
             BigNumber,
             CorrectNumber
         }
-
     }
 }
--- LineCounting/LineCounting/CountLines.cs
+++ LineCounting/LineCounting/CountLines.cs
@@ -3,14 +3,10 @@ using System.IO;
 using System.Linq;
 using static System.String;
 
-namespace LineCounting
-{
-    internal class CountLines
-    {
-        private static void Main(string[] args)
-        {
-            if (args.Length == 0)
-            {
+namespace LineCounting{
+    internal class CountLines{
+        private static void Main(string[] args){
+            if (args.Length == 0){
                 Console.WriteLine("Not enough arguments, enter extension.");
                 return;
             }
@@ -18,33 +14,29 @@ namespace LineCounting
             var numberOfLines = countLines.Count();
             Console.WriteLine($"Number of lines: {numberOfLines}");
         }
-        private int Count()
-        {
+
+        private int Count(){
             var files = Directory.GetFiles(_directory, _extension, SearchOption.AllDirectories);
             return files.Sum(CountLinesInFile);
         }
 
-        private int CountLinesInFile(string file)
-        {
+        private int CountLinesInFile(string file){
             var result = 0;
-            using (var fileReader = new StreamReader(file))
-            {
-                while (!fileReader.EndOfStream)
-                {
+            using (var fileReader = new StreamReader(file)){
+                while (!fileReader.EndOfStream){
                     var line = fileReader.ReadLine();
                     if (line == null) continue;
                     line = DeleteComments(line);
-                    if (!IsNullOrEmpty(line)) result++; 
+                    if (!IsNullOrEmpty(line)) result++;
                 }
             }
-           return result;
+
+            return result;
         }
 
-        private string DeleteComments(string line)
-        {
-            line = line.Trim();  
-            if (_isInComment)
-            {
+        private string DeleteComments(string line){
+            line = line.Trim();
+            if (_isInComment){
                 if (!line.Contains(MultiLineCloseComment)) return null;
                 line = line.Remove(0,
                     line.IndexOf(MultiLineCloseComment, StringComparison.Ordinal) + MultiLineCloseComment.Length);
@@ -52,33 +44,31 @@ namespace LineCounting
                 return DeleteComments(line);
             }
 
-            if (line.StartsWith(SingleLineComment) || 
+            if (line.StartsWith(SingleLineComment) ||
                 (line.StartsWith(MultiLineOpenComment) && line.EndsWith(MultiLineCloseComment))) return null;
-            if(line.Contains(MultiLineOpenComment) )
-            {
-                if (!line.Contains(MultiLineCloseComment))
-                {
+            if (line.Contains(MultiLineOpenComment)){
+                if (!line.Contains(MultiLineCloseComment)){
                     _isInComment = true;
                     var index = line.IndexOf(MultiLineOpenComment, StringComparison.Ordinal);
-                    line = line.Remove(index, line.Length - index );   
+                    line = line.Remove(index, line.Length - index);
                 }
-                else
-                {
+                else{
                     var index = line.IndexOf(MultiLineOpenComment, StringComparison.Ordinal);
-                    line = line.Remove(index, 
-                        line.IndexOf(MultiLineCloseComment, StringComparison.Ordinal) + MultiLineCloseComment.Length - index);
+                    line = line.Remove(index,
+                        line.IndexOf(MultiLineCloseComment, StringComparison.Ordinal) + MultiLineCloseComment.Length -
+                        index);
                     return DeleteComments(line);
-                }  
+                }
             }
+
             return line;
         }
 
-        private CountLines(string extension)
-        {
+        private CountLines(string extension){
             _extension = extension;
             _directory = Directory.GetCurrentDirectory();
         }
-        
+
         private readonly string _extension;
         private readonly string _directory;
         private bool _isInComment;
