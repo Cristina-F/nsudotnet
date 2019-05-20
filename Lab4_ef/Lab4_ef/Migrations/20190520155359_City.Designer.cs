fceedeedd4a10c166db90962a96ed8507c133fbb
Cristina-F <k.fefelova@g.nsu.ru>
2019-05-20 22:59:40 +0700

    add city

--- Lab4_ef/Lab4_ef/Migrations/20190520155359_City.Designer.cs
+++ Lab4_ef/Lab4_ef/Migrations/20190520155359_City.Designer.cs
@@ -4,14 +4,16 @@ using Lab4_ef.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Lab4_ef.Migrations
 {
     [DbContext(typeof(MyContext))]
-    partial class MyContextModelSnapshot : ModelSnapshot
+    [Migration("20190520155359_City")]
+    partial class City
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Lab4_ef/Lab4_ef/Migrations/20190520155359_City.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lab4_ef.Migrations
+{
+    public partial class City : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Employees_EmployeeId",
+                table: "Projects");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EmployeeId",
+                table: "Projects",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Employees",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Employees_EmployeeId",
+                table: "Projects",
+                column: "EmployeeId",
+                principalTable: "Employees",
+                principalColumn: "EmployeeId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Employees_EmployeeId",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Employees");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EmployeeId",
+                table: "Projects",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Employees_EmployeeId",
+                table: "Projects",
+                column: "EmployeeId",
+                principalTable: "Employees",
+                principalColumn: "EmployeeId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
