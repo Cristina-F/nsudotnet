ea76a89c162ebdfde13150a8bf6d30dd19e80ff5
Cristina-F <k.fefelova@g.nsu.ru>
2019-05-20 22:00:43 +0700

    add age

--- Lab4_ef/Lab4_ef/Models/EmployeeView.cs
+++ Lab4_ef/Lab4_ef/Models/EmployeeView.cs
@@ -9,6 +9,7 @@ namespace Lab4_ef.Models {
         public int EmployeeId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public int Age { get; set; }
         public int Sum { get; set; }
         
     }
